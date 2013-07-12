9f336e0e2c5492842945144bf37e62508424f53b
若槻祐彰 <yoshiaki@wakatsukiyoshiaki-no-MacBook-Pro.local>
2013-07-12 16:31:44 +0900

    switch default break

--- src/main/java/twitter4j/internal/http/BASE64Encoder.java
+++ src/main/java/twitter4j/internal/http/BASE64Encoder.java
@@ -63,6 +63,9 @@ public final class BASE64Encoder {
                             currentByte |= (from[i + 1] & lead4byte) >>> 4;
                         }
                         break;
+                        
+                        default:
+                        	break;
                 }
                 to.append(encodeTable[currentByte]);
                 num += 6;
